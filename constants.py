90b751a9e22211683d911987e740aebc99053388
Ben <benmega@gmail.com>
2025-08-31 10:23:06 +0700

    refactor: split main.py into modules

---
+++ constants.py
@@ -0,0 +1,35 @@
+# Constants for the plane simulation
+
+
+# Screen setup
+WIDTH = 800
+HEIGHT = 600
+
+# Colors
+SKY = (135, 206, 235)
+GRASS = (34, 139, 34)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+RUNWAY_COLOR = (60, 60, 60)
+
+# Physics constants
+GRAVITY = 0.05
+TILT_SPEED = 0.3
+MAX_SAFE_TILT = 15
+MAX_TILT = 25
+WIND_SPEED = 10
+PLANE_WEIGHT = 100
+
+# Speed constants
+MIN_KPH = 0
+MAX_KPH = 240
+SAFE_LANDING_KPH = 150
+SAFE_VERTICAL_SPEED = 1.5
+
+# Cloud constants
+NUM_CLOUDS = 8
+
+# Runway constants
+RUNWAY_LENGTH = 400
+RUNWAY_GAP = 2000
